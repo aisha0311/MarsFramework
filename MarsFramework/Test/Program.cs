df304df5e451047b37d645cdf151f649313eb918
aisha0311 <shaikaishatasleem@gmail.com>
2019-11-21 10:07:22 +1300

    first commit

---
+++ MarsFramework/Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using MarsFramework.Global;
+using MarsFramework.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace MarsFramework
+{
+    public class Program
+    {
+
+        
+        [TestFixture]
+        [Category("Sprint1")]
+        class User : Global.Base
+        {
+          
+            [Test]
+            public void Test()
+            {
+                SignIn signIn = new SignIn();
+                signIn.LoginSteps();
+
+            }
+
+
+
+        }
+    }
+}
